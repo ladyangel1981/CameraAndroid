5e2f0076a2ea2b4adb20bf0de496a1a9a7aaeb8d
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2021-10-22 18:00:40 +0200

    update

--- app/src/main/java/com/example/cameraandroid/MainActivity.java
+++ app/src/main/java/com/example/cameraandroid/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.cameraandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ActivityNotFoundException;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,46 +32,52 @@ public class MainActivity extends AppCompatActivity {
                 dispatchTakePictureIntent();
             }
         });
-
-
     }
 
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    public void dispatchTakePictureIntent(){
+    private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this,"no es pot obrir la camara",Toast.LENGTH_SHORT).show();
+        // Ensure that there's a camera activity to handle the intent
+        //if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = getFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
-
-
+    protected File getFile() throws IOException{
+        //guardar a un fitxer
+        File path =getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //para que este en el almacenamiento externo
+        File foto= new File(path,"imatge.jpg");
+        return foto;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         final ImageView imageView = findViewById(R.id.imageView);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-            imageView.setImageBitmap(imageBitmap);
-
-            //Guardar a un fitxer
-            // getExternalFilesDir() -- para guardar archivos external dir
-
-            File path = getApplicationContext().getFilesDir();
-            File path2 = new File(path, "foto.jpg");
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream(path2);
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+        try {
+            Uri fileuri = Uri.fromFile( getFile() );
+            imageView.setImageURI(fileuri);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
+
 }
